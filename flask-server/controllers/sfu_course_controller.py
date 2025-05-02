d7133c45641481321e531389e302a788db43a1f8
Andy-J-B <080.abae@gmail.com>
2025-05-02 12:07:36 -0700

    Done transcript extractor

--- flask-server/controllers/sfu_course_controller.py
+++ flask-server/controllers/sfu_course_controller.py
@@ -51,6 +51,11 @@ def get_sfu_courses() -> dict:
             for course in response.json():
                 if int(re.sub(r"[^\d]", "", course["text"])) > 500:
                     continue
+                elif "title" in course and (
+                    "Practicum" in course["title"]
+                    or "Research Project" in course["title"]
+                ):
+                    continue
                 else:
                     sfu_courses[department].append(course)
         except requests.exceptions.RequestException as e:
@@ -66,23 +71,108 @@ def get_course_sections():
 
     Returns the dict of course sections for each course in form :
 
+    {
+        <string (department)> : {
+            <string (course_number)> : [
+                <dict (course) >
+            ]
+        }
+    }
+
     """
 
-    return
+    course_dict = get_sfu_courses()
+
+    for department, courses in course_dict.items():
+        course_section_dict = {}
+        if department != "CMPT":
+            course_dict[department] = course_section_dict
+            continue
+        for course in courses:
+            response = requests.get(
+                COURSE_API_URL + "/" + department + "/" + course["text"]
+            )
+            sections = response.json()
+
+            grouped_sections = {}
+            for section in sections:
+                if response.status_code == 404:
+                    continue
+
+                associated_number = section["associatedClass"]
+                if associated_number not in grouped_sections:
+                    grouped_sections[associated_number] = []
+
+                grouped_sections[associated_number].append(section)
+            course_section_dict[course["text"]] = grouped_sections
+        course_dict[department] = course_section_dict
+
+    return course_dict
 
 
 def get_course_outlines():
-    return
+    """
+    Send Get request to SFU Courses API to
+    fetch course outlines for each course listed for this semester.
+    update or add to the database
+
+    Returns the dict of course sections for each course in form :
+
+    {
+        <string (department)> : {
+            <string (course_number)> : [
+                <dict (course) >
+            ]
+        }
+    }
+
+    """
+
+    course_dict = get_sfu_courses()
+
+    for department, courses in course_dict.items():
+        course_section_dict = {}
+        if department != "CMPT":
+            course_dict[department] = course_section_dict
+            continue
+        for course in courses:
+            response = requests.get(
+                COURSE_API_URL + "/" + department + "/" + course["text"]
+            )
+            sections = response.json()
+
+            grouped_sections = {}
+            for section in sections:
+                if response.status_code == 404:
+                    continue
+
+                associated_number = section["associatedClass"]
+                if associated_number not in grouped_sections:
+                    grouped_sections[associated_number] = []
+
+                grouped_sections[associated_number].append(section)
+            course_section_dict[course["text"]] = grouped_sections
+        course_dict[department] = course_section_dict
+
+    return course_dict
 
 
-# def get_sfu_courses():
-#     """Fetch courses from an external API"""
-#     try:
-#         response = requests.get(COURSE_API_URL)
-#         response.raise_for_status()  # Raise an error for HTTP errors
-#         return response.json()  # Return JSON response
-#     except requests.exceptions.RequestException as e:
-#         return {"error": str(e)}
+def post_db():
+    """
+    Make the database using the given course options.
+    For each section of each course of each department
+    Get the course outline, make a database element
+    """
+
+    #
 
 
-print(get_sfu_courses())
+def get_db():
+    """"""
+
+
+def update_db():
+    """"""
+
+
+print(get_course_outlines())
--- flask-server/controllers/transcript_controller.py
+++ flask-server/controllers/transcript_controller.py
@@ -1,1 +1,127 @@
 # Logic for extracting transcript text
+
+import pdfplumber
+import re, json
+
+laparams_settings = {
+    "line_overlap": 0.3,  # Controls how much overlap is considered a single line
+    "char_margin": 0.5,  # Merges characters into words if they are close
+    "word_margin": 0.1,  # Merges words if they are close
+    "line_margin": 0.1,  # Merges text into lines if they are close
+    "boxes_flow": 1,  # Controls the detection of text columns
+}
+
+with pdfplumber.open("UT.pdf", laparams=laparams_settings) as pdf:
+    text = []
+    start_of_courses = False
+
+    for page in pdf.pages:
+        for line in page.extract_text_lines():
+
+            # Find which major
+            if line["text"].strip().startswith("Major in "):
+                major = line["text"]
+
+            # Find which lines are courses
+            if line["text"].strip().startswith("Attempted"):
+                start_of_courses = True
+                continue
+            elif start_of_courses and line["text"].startswith("Term"):
+                start_of_courses = False
+            if line["text"] and start_of_courses and len(line["text"]) > 3:
+                text.append(line["text"])
+
+    courses = "\n".join(text)
+
+# print(courses)
+# print(major)
+
+
+def parse_course_data(text_list: list) -> dict:
+    """
+    Parse course data obtained from transcript PDF.
+    Returns a dictionary with each course taken.
+
+    Dictionary structure:
+    {
+        key: {
+            "course_department": <string> (All Caps),
+            "course_number": <int>,
+            "course_name": <string>,
+            "units_attempted": <int>,
+            "units_completed": <int>,
+            "grade": <string> or None,
+            "grade_points": <float>,
+            "class_average": <string> or None,
+            "class_enrollment": <int>
+        },
+        ...
+    }
+    """
+
+    course_dict = {}
+
+    for key, course in enumerate(text_list):
+        course_list = extract_course_info(course)
+
+        # Unpacking values from course_list
+        (
+            course_department,
+            course_number,
+            course_name,
+            units_attempted,
+            units_completed,
+            *rest,
+        ) = course_list
+
+        # Check for missing grade case
+        if rest[-2] == "-":
+            grade, class_average = None, None
+            grade_points, class_enrollment = rest[0], rest[2]
+        else:
+            grade, grade_points, class_average, class_enrollment = rest
+
+        # Populate dictionary
+        print(course_list, class_enrollment, grade_points)
+        course_dict[key] = {
+            "course_department": course_department,
+            "course_number": course_number,
+            "course_name": course_name,
+            "units_attempted": float(re.sub(r"[^\d.]", "", units_attempted)),
+            "units_completed": float(re.sub(r"[^\d.]", "", units_completed)),
+            "grade": grade,
+            "grade_points": float(re.sub(r"[^\d.]", "", grade_points)),
+            "class_average": class_average,
+            "class_enrollment": int(re.sub(r"[^\d.]", "", class_enrollment)),
+        }
+
+    return course_dict
+
+
+def extract_course_info(s):
+    # Extract first 2 : department and number
+    first_parts = s.split(" ", 2)
+
+    # Extract last 6 : units attempted, units completed, grade, grade points, average, enrollment
+    last_parts = first_parts[-1].rsplit(" ", 6)
+
+    final_course_list = first_parts[:2] + last_parts
+
+    if final_course_list[-2] == "-":
+
+        final_course_list = (
+            first_parts[:2]
+            + [final_course_list[2] + " " + final_course_list[3]]
+            + final_course_list[4:]
+        )
+
+    return final_course_list
+
+
+course_data_dict = parse_course_data(text)
+
+# Convert Python to JSON
+json_object = json.dumps(course_data_dict, indent=4)
+
+# Print JSON object
+print(json_object)
